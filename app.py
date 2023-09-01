f8843b8c3b12c4c0075677bb66b7fe6d52e9e919
MANIKANDAN A <Veera-Ruki@users.noreply.huggingface.co>
2023-09-01 10:17:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def login_section():
         
 
 
-def predict():
+def predict(cap_col):
     captions = []
     pred_caption = generate_caption('tmp.jpg', caption_model)
 
@@ -118,41 +118,6 @@ def predict():
     for c in captions:
         cap_col.markdown(f'<div class="cap-line" style="color: black; background-color: light grey; padding: 5px; margin-bottom: 5px; font-family: \'Palatino Linotype\', \'Book Antiqua\', Palatino, serif;">{c}</div>', unsafe_allow_html=True)
     cap_col.markdown('</div>', unsafe_allow_html=True)
-
-st.markdown('<h1 style="text-align:center; font-family:Arial; width:fit-content; font-size:3em; color:black; text-shadow: 2px 2px 4px #000000;">IMAGE CAPTION GENERATOR</h1>', unsafe_allow_html=True)
-col1, col2 = st.columns(2)
-
-# Image URL input
-img_url = st.text_input(label='Enter Image URL')
-
-# Image upload input
-img_upload = st.file_uploader(label='Upload Image', type=['jpg', 'png', 'jpeg'])
-
-# Language selection dropdown
-target_language = st.selectbox('Select Target Language', ['en', 'ta', 'hi', 'es', 'fr', 'zh-cn'], index=0)
-
-# Process image and generate captions
-if img_url:
-    img = Image.open(requests.get(img_url, stream=True).raw)
-    img = img.convert('RGB')
-    col1.image(img, caption="Input Image", use_column_width=True)
-    img.save('tmp.jpg')
-    predict(col2)
-
-    st.markdown('<center style="opacity: 70%">OR</center>', unsafe_allow_html=True)
-
-elif img_upload:
-    img = img_upload.read()
-    img = Image.open(io.BytesIO(img))
-    img = img.convert('RGB')
-    col1.image(img, caption="Input Image", use_column_width=True)
-    img.save('tmp.jpg')
-    predict(col2)
-
-# Remove temporary image file
-if img_url or img_upload:
-    os.remove('tmp.jpg')
-    
     
 
 def main():
@@ -171,7 +136,40 @@ def main():
     elif selected_tab == "Login":
         login_section()
     elif selected_tab == "Generate Caption":
-        predict()                
+        st.markdown('<h1 style="text-align:center; font-family:Arial; width:fit-content; font-size:3em; color:black; text-shadow: 2px 2px 4px #000000;">IMAGE CAPTION GENERATOR</h1>', unsafe_allow_html=True)
+        col1, col2 = st.columns(2)
+
+# Image URL input
+        img_url = st.text_input(label='Enter Image URL')
+
+# Image upload input
+        img_upload = st.file_uploader(label='Upload Image', type=['jpg', 'png', 'jpeg'])
+
+# Language selection dropdown
+        target_language = st.selectbox('Select Target Language', ['en', 'ta', 'hi', 'es', 'fr', 'zh-cn'], index=0)
+
+# Process image and generate captions
+        if img_url:
+           img = Image.open(requests.get(img_url, stream=True).raw)
+           img = img.convert('RGB')
+           col1.image(img, caption="Input Image", use_column_width=True)
+           img.save('tmp.jpg')
+           predict(col2)
+
+           st.markdown('<center style="opacity: 70%">OR</center>', unsafe_allow_html=True)
+
+        elif img_upload:
+             img = img_upload.read()
+             img = Image.open(io.BytesIO(img))
+             img = img.convert('RGB')
+             col1.image(img, caption="Input Image", use_column_width=True)
+             img.save('tmp.jpg')
+             predict(col2)
+
+# Remove temporary image file
+        if img_url or img_upload:
+           os.remove('tmp.jpg')
+                        
     else:
         st.write("Please login to access this feature.")
 
